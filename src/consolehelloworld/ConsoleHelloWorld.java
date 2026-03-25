ea11e35532ad1d445a450e1265140cd2aaa27ba7
Thobani <Thobani@YA13Q50-LTE>
2026-03-25 22:18:04 +0200

    Initial commit of my Java project

---
+++ src/consolehelloworld/ConsoleHelloWorld.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package consolehelloworld;
+
+/**
+ *
+ * @author Faranani
+ */
+public class ConsoleHelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    static void qtnOutput(){
+    System.out.println("Hello World!");    
+    }
+    public static void main(String[] args) {
+    qtnOutput();
+    
+    // TODO code application logic here
+    }
+    
+}
